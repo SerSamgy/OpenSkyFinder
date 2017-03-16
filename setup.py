ed6e7f54ea88dc433c0f394fdb0e7dad50df208f
Semyon Gaivoronskiy <samgy7cool@gmail.com>
2017-03-16 17:31:07 +0300

    Fixes in setup files

--- setup.py
+++ setup.py
@@ -15,7 +15,10 @@ from setuptools import setup
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['pytest-runner'] + sphinx, tests_require=['pytest'],)
+    setup(
+        setup_requires=['pytest-runner'] + sphinx,
+        tests_require=['pytest', 'pytest-cov'],
+        install_requires=['haversine==0.4.5', 'requests==2.13.0'])
 
 
 if __name__ == "__main__":
